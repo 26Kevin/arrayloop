53dabb03b29c54d45692c98455aa754413a8622b
26Kevin <113374266+26Kevin@users.noreply.github.com>
2023-03-06 11:15:09 -0700

    updating

---
+++ main.js
@@ -0,0 +1,10 @@
+// Create array - Using Loops
+
+// Main Algorith:
+// Start with an empty array
+// Loop - inside of the loop we add elements to the array
+
+let happyArray = [];
+for (let n = 1; n <= 100; n++) {
+    happyArray.push(randomDec(5, 8)); 
+}
